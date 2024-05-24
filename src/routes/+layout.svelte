77028130a4fcab241aa290328401919bd9dcbebf
Aritra <aritra041999.dev@gmail.com>
2024-05-24 11:28:05 +0530

    feat: Update layout to include navigation bar and main content section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
 	import '../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+	import Nav from '$lib/components/ui/nav/nav.svelte';
 </script>
 
-<slot />
+<ModeWatcher />
+<main class="flex">
+	<Nav />
+	<section class="h-screen w-[calc(100vw-4rem)] p-4">
+		<slot />
+	</section>
+</main>
