329e86ed196d66a66f3718d9497d586bc8578746
Aritra <aritra041999.dev@gmail.com>
2024-05-26 10:36:42 +0530

    feat: Add Kubernetes client configuration and API client

---
+++ src/lib/k8s.ts
@@ -0,0 +1,5 @@
+import * as k8s from '@kubernetes/client-node';
+
+export const k8sClient = new k8s.KubeConfig();
+k8sClient.loadFromDefault();
+export const k8sApi = k8sClient.makeApiClient(k8s.CoreV1Api);
